5264ed14dc2244eb1e93c53e9fa3cb607d5eb099
jenna <kobularj@go.stockton.edu>
2022-11-02 15:00:54 -0400

    select from tables

--- src/clients/config.py
+++ src/clients/config.py
@@ -17,40 +17,6 @@ def config(filename='.config/database.ini', section='postgresql'):
 		raise Exception('Section {0} not found in the {1} file'.format(section, filename))
 
 	return db
-import psycopg
-from config import config
-
-def connect():
-	""" Connect to the PostgreSQL database server """
-	conn = None
-	try:
-		# read connection parameters
-		params = config()
-
-		# connect to the PostgreSQL server
-		print('Connecting to the PostgreSQL database...')
-		conn = psycopg.connect(**params)
-		
-		# create a cursor
-		cur = conn.cursor()
-		
-	# execute a statement
-		print('PostgreSQL database version:')
-		cur.execute('SELECT version()')
-
-		# display the PostgreSQL database server version
-		db_version = cur.fetchone()
-		print(db_version)
-		
-	# close the communication with the PostgreSQL
-		cur.close()
-	except (Exception, psycopg.DatabaseError) as error:
-		print(error)
-	finally:
-		if conn is not None:
-			conn.close()
-			print('Database connection closed.')
 
 
-if __name__ == '__main__':
-	connect()
+
--- src/tasks.py
+++ src/tasks.py
@@ -1,17 +1,48 @@
 import pandas as pd
-from psycopg import Connection
-from clients.postgres import PostgresClient
+from clients.config import config
+import psycopg
 
-DATABASECONFIG = '.config/database.ini'
+def connect():
+	""" Connect to the PostgreSQL database server """
+	conn = None
+	try:
+		# read connection parameters
+		params = config()
 
-def setup_client(config_file, section):
-    client=PostgresClient
-    cursor= client.connect_from_config(path=config_file, section=section)
-    return cursor 
+		# connect to the PostgreSQL server
+		print('Connecting to the PostgreSQL database...')
+		conn = psycopg.connect(**params)
+		
+		# create a cursor
+		cur = conn.cursor()
+		
+	# execute a statement
+		print('PostgreSQL database version:')
+		cur.execute('SELECT version()')
 
-def select_from_table(cursor, table_name:str, schema:str):
-    cursor.execute(f"SET search_path To {schema}, public;")
-    results = cursor.execute(f"SELECT * FROM {table_name};").fetchall()
-    print(results)
+		# display the PostgreSQL database server version
+		db_version = cur.fetchone()
+		print(db_version)
+		
+	# close the communication with the PostgreSQL
+		cur.close()
+	except (Exception, psycopg.DatabaseError) as error:
+		print(error)
+	finally:
+		if conn is not None:
+			conn.close()
+			print('Database connection closed.')
 
+
+if __name__ == '__main__':
+	connect()
+ 
+def select_from_table(cur, table_name, schema):
+  params = config()
+  conn = psycopg.connect(**params)
+  cur = conn.cursor()
+  cur.execute(f"SET search_path TO {schema}, public;")
+  results= cur.execute(f"SELECT * FROM {table_name};").fetchall()
+  conn.close()
+  return results
 
