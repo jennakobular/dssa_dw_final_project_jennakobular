c535997505d1e8dc8ed5a238eceabc8ae36121ab
jenna <kobularj@go.stockton.edu>
2022-11-01 20:59:18 -0400

    updates

--- src/tasks.py
+++ src/tasks.py
@@ -1,1 +1,17 @@
-from abc import ABCMeta, abstractclassmethod
+import pandas as pd
+from psycopg import Connection
+from clients.postgres import PostgresClient
+
+DATABASECONFIG = '.config/database.ini'
+
+def setup_client(config_file, section):
+    client=PostgresClient
+    cursor= client.connect_from_config(path=config_file, section=section)
+    return cursor 
+
+def select_from_table(cursor, table_name:str, schema:str):
+    cursor.execute(f"SET search_path To {schema}, public;")
+    results = cursor.execute(f"SELECT * FROM {table_name};").fetchall()
+    print(results)
+
+
