ea150a8a59bd93ea938acae578d07ce037bcdc18
jenna <kobularj@go.stockton.edu>
2022-11-13 18:39:57 -0500

    added finalproject with further instructions

---
+++ final_project.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import psycopg
+from src.clients.config import config
+
+"""" USE PYPIKA FOR POSTGRESQL BUILDER  - looks like will need to pip install
+from pypika import PostgreSQLQuery
+from pypika import Query, Schema, Column
+
+will need to import MY pipeline & MY Task & Conifg
+
+this is where I'll build out my DSSA Schema ie
+
+fact_rental = (
+    Column('sk_customer', 'INT', False)
+     ...
+    ....
+    
+)
+
+in worker vid this is ~16min in -- I am not here yet w. my project, will need to revisit
+
+
+
+"""
+
+""" HERE FUNCTIONS WILL GO"""
+
+def create_cursor():
+    params=config()
+    conn= psycopg.connect(**params)
+    cur = conn.cursor()
+    return cur
+
+def create_schema(cur, scehma_name):
+    q = f"CREATE SCHEMA IF NOT EXISTS (schema_name);"
+    cur.execute(q)
+    return cur
+
+"""18min in, create table, need pypika installed so i can actually utilize"""
+"""stopped @20min in, need to get caught up to this point"""
--- src/common/queues.py
+++ src/common/queues.py
@@ -1,16 +1,16 @@
 from asyncio import Queue as AsyncQueue
-from queue import Queue as Queue
+from queue import Queue as ThreadSafeQueue
 from multiprocessing import JoinableQueue 
 from typing import Union
 
 class QueueFactory:
     
     @staticmethod
-    def factory(type: str = 'default') -> Union[Queue, AsyncQueue, JoinableQueue]:
+    def factory(type: str = 'default') -> Union[ThreadSafeQueue, AsyncQueue, JoinableQueue]:
         if type == 'default':
-            return Queue()
+            return ThreadSafeQueue()
         elif type == 'multi-threading':
-            return Queue()
+            return ThreadSafeQueue()
         elif type == 'multi-processing':
             return JoinableQueue()
         elif type == 'asyncio':
