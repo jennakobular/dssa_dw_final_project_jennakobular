61163f53a03b6ed494ab1cfcba6cef8e4f951af4
jenna <kobularj@go.stockton.edu>
2022-11-15 20:28:26 -0500

    updates from zoom extra help

--- src/main.py
+++ src/main.py
@@ -1,3 +1,14 @@
 #entry point, main script that will build star schema for project
 #this is where data processing script goes
-import networkx as nx
+from clients.config import config
+import psycopg
+from tasks import Task
+
+def connect_to_dvdrental():
+    params=config()
+    conn= psycopg.connect(**params)
+    cur = conn.cursor()
+    return cur
+
+task0=Task(connect_to_dvdrental)
+task0.run()
--- src/tasks.py
+++ src/tasks.py
@@ -1,22 +1,34 @@
 import pandas as pd
 from clients.config import config
 import psycopg
-from datetime import datetime as dt 
- 
-def connect_to_dvdrental():
+
+
+class Task():
+    
+    def __init__(self, func) -> None:
+        self.func = func
+        
+    def run(self, *args, **kwargs):
+        result = self.func(*args, **kwargs)
+        return result
+
+
+    """ these are functions that go in MAIN
+    
+    def connect_to_dvdrental():
     params=config()
     conn= psycopg.connect(**params)
     cur = conn.cursor()
     return cur
  
  
-""" task to select from database table"""
+ task to select from database table
  
 def select_from_table(cur, table_name):
     results= cur.execute(f"SELECT * FROM {table_name};").fetchall()
     return results
 
-"""task to convert result to a dataframe"""
+task to convert result to a dataframe
 
 def convert_pandas(results):
     df = pd.DataFrame(results)
@@ -25,7 +37,7 @@ def convert_pandas(results):
 
 def main():
     
-   """ 1) need to figure out what to put in here (def main()) -- it seems like this area CAN be hardcoded but unsure of syntax to use still. pg 222 in data wrangling has example of def main()
+   1) need to figure out what to put in here (def main()) -- it seems like this area CAN be hardcoded but unsure of syntax to use still. pg 222 in data wrangling has example of def main()
    
  2) need to figure out how to run through the nec. table names for select_from_table() funct. --> i believe this is touched upon in worker video
  
@@ -33,8 +45,9 @@ def main():
  
  4) first function connect_to_dvd definitely works
  
- 5) need to figure out if i should delete tables from pgadmin and just create in code -- i think that may make more sense for me"""
+ 5) need to figure out if i should delete tables from pgadmin and just create in code -- i think that may make more sense for me
 
 if __name__ == "__main__":
     main()
- 
+    
+"""
