ce6bb5aee42d57a6b6b5235f8dbae212fec87dc6
jenna <kobularj@go.stockton.edu>
2022-11-16 18:48:42 -0500

    updates to task & main

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@
 from clients.config import config
 import psycopg
 from tasks import Task
+import pandas as pd
 
 def connect_to_dvdrental():
     params=config()
@@ -12,3 +13,14 @@ def connect_to_dvdrental():
 
 task0=Task(connect_to_dvdrental)
 task0.run()
+
+def select_from_table(cur, table_name):
+    results= cur.execute(f"SELECT * FROM {table_name};").fetchall()
+    return results
+
+def convert_pandas(results):
+    df = pd.DataFrame(results)
+    return df
+
+task1=Task(select_from_table)
+task1.run(cur= task0, table_name='actor')
